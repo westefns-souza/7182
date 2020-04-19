124b2f1581ff7e77f4f3eb6691868289b93118f2
westefns-souza <westefns@outlook.com.br>
2020-04-19 14:45:24 -0300

    Movendo o Order de Entity.cs para Order.cs

--- Store.Domain/Entities/Entity.cs
+++ Store.Domain/Entities/Entity.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using Store.Domain.Enums;
 
 namespace Store.Domain.Entities
 {
@@ -12,60 +10,5 @@ namespace Store.Domain.Entities
         }
 
         public Guid Id { get; private set; }
-    }
-
-    public class Order : Entity
-    {
-        public Order(Customer custumer, decimal deliveryFee, Discount discount)
-        {
-            Custumer = custumer;
-            Date = DateTime.Now;
-            Number = Guid.NewGuid().ToString().Substring(0, 8);
-            Items = new List<OrderItem>();
-            DeliveryFee = deliveryFee;
-            Discount = discount;
-            Status = EOrderStatus.WaitingPayment;
-        }
-
-        public Customer Custumer { get; private set; }
-        public DateTime Date { get; private set; }
-        public string Number { get; private set; }
-        public IList<OrderItem> Items { get; private set; }
-        public decimal DeliveryFee { get; private set; }
-        public Discount Discount { get; private set; }
-        public EOrderStatus Status { get; private set; }
-
-        public void AddItem(Product product, int quantity)
-        {
-            var item = new OrderItem(product, quantity);
-        }
-
-        public decimal Total()
-        {
-            decimal total = 0;
-
-            foreach (var item in Items)
-            {
-                total += item.Total();
-            }
-
-            total += DeliveryFee;
-            total -= Discount != null ? Discount.Value() : 0;
-
-            return total;
-        }
-
-        public void Pay(decimal amount)
-        {
-            if (amount == Total())
-            {
-                Status = EOrderStatus.WaitingDelivery;
-            }
-        }
-
-        public void Cancel()
-        {
-            Status = EOrderStatus.Canceled;
-        }
     }
 }
