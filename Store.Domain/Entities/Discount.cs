078d4a45fcc5707c24a3cb2c5ad956af652ee9f3
westefns-souza <westefns@outlook.com.br>
2020-04-19 14:14:50 -0300

    Entidade de Desconto

---
+++ Store.Domain/Entities/Discount.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Store.Domain.Entities
+{
+    public class Discount : Entity
+    {
+        public Discount(decimal amount, DateTime expiresDate)
+        {
+            Amount = amount;
+            ExpiresDate = expiresDate;
+        }
+
+        public decimal Amount { get; private set; }
+        public DateTime ExpiresDate { get; private set; }
+
+        public bool IsValid()
+        {
+            return DateTime.Compare(DateTime.Now, ExpiresDate) < 0;
+        }
+
+        public decimal Valus()
+        {
+            return IsValid() ? Amount : 0;
+        }
+    }
+}
