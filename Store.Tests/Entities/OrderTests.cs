5f282bb38a85de2bd6bfdf52c99b33b71be7ed0a
westefns-souza <westefns@outlook.com.br>
2020-04-19 18:44:43 -0300

    Testando o status do pagamento

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -1,27 +1,38 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Store.Domain.Entities;
+using Store.Domain.Enums;
 
 namespace Store.Tests.Domain
 {
     [TestClass]
     public class OrderTests
     {
+        private readonly Customer _costumer;
+        private readonly Product _product;
+        private readonly Discount _discount;
+        private readonly Order _order;
+
+        public OrderTests()
+        {
+            _costumer = new Customer("Westefns", "westefns@outlook.com.br");
+            _product = new Product("Produto 1", 10, true);
+            _discount = new Discount(10, DateTime.Now.AddDays(5));
+            _order = new Order(_costumer, 0, _discount);
+        }
+
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
         {
-            var costumer = new Customer("Westefns", "westefns@outlook.com.br");
-            var order = new Order(costumer, 0, null);
-
-            Assert.AreEqual(8, order.Number.Length);
+            Assert.AreEqual(8, _order.Number.Length);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
         {
-            Assert.Fail();
+            Assert.AreEqual(EOrderStatus.WaitingPayment, _order.Status);
         }
 
         [TestMethod]
