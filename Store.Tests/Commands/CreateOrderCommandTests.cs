d989f2930cee7c4b2cdfacdfbf052a1615b9cacc
westefns-souza <westefns@outlook.com.br>
2020-04-19 22:05:04 -0300

    Testanto os Comandos

---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Commands;
+
+namespace Store.Tests.Commands
+{
+    [TestClass]
+    public class CreateOrderCommandTests
+    {
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado()
+        {
+            var command =  new CreateOrderCommand
+            {
+                Customer = "",
+                ZipCode = "593380000",
+                PromoCode = "12345678",
+            };
+
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.AreEqual(command.Invalid, true);
+        }
+    }
+}
